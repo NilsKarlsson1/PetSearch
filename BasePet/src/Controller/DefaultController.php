80d403582e957144fd1708ad696d66faa9fda39e
NilsKarlsson1 <nils.karlsson@imie-paris.fr>
2019-11-29 15:43:45 +0100

    page de login + redirections

--- BasePet/src/Controller/DefaultController.php
+++ BasePet/src/Controller/DefaultController.php
@@ -25,4 +25,41 @@ class DefaultController extends AbstractController{
             "users" => $users
             ]);
     }
+
+    public function contact():Response
+    {
+            //BDD
+        $users = [
+            [ "name" => "Ivan" , "poste" => "j ai"],
+            [ "name" => "Sacard" , "poste" => "pas d "],
+            [ "name"=>"Havane","poste"=>"inspiration"],
+            
+        ];
+
+
+
+        return $this->render('contact.html.twig', [
+            'title' => "crash",
+            "users" => $users
+            ]);
+    }
+
+
+    public function login():Response
+    {
+            //BDD
+        $users = [
+            [ "name" => "Ivan" , "poste" => "j ai"],
+            [ "name" => "Sacard" , "poste" => "pas d "],
+            [ "name"=>"Havane","poste"=>"inspiration"],
+            
+        ];
+
+
+
+        return $this->render('login.html.twig', [
+            'title' => "crash",
+            "users" => $users
+            ]);
+    }
 }
