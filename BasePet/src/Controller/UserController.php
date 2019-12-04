c87951a4a477ab02f16e421d4a94242c8962e83f
Maysoon <maysoondoucanse@gmail.com>
2019-12-04 11:49:14 +0100

    ajout des requetes de recherche(sexe, type email, same country )

--- BasePet/src/Controller/UserController.php
+++ BasePet/src/Controller/UserController.php
@@ -4,10 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController{
 
@@ -15,17 +14,57 @@ class UserController extends AbstractController{
      * @ var UserRepository
      */
     
-    public function user(EntityManagerInterface $em, UserRepository $repository):Response
+    public function user(UserRepository $repository):Response
     {
-            //BDD
-        //$repository = $em->getRepository(User::class);
+            //la liste des utilisateur
         $listUser= $repository->listAllUser(1);
-        /*->createQueryBuilder('u')
-        ->select('count(u.email) as mailType, u.iduser, u.lastname, u.firstname, u.city')
-        ->getQuery()
-        ->getResult();*/
-
+        
         return $this->render('user.html.twig', ['listuser' => $listUser]);
      }
+
+     /**
+      * Le nombre d’utilisateur par ville, commun
+      */
+     public function sameCountry (UserRepository $repository, Request $request):Response
+     {
+       $country= $request->query->get('country', 'France');
+       $userSameCountry = $repository->userSameCountry($country);
+
+       return $this->render('user.html.twig', ['userSameCountry' => $userSameCountry]);
+     }
+
+     public function  numberNewRegister (UserRepository $repository) 
+     {
+        //$nbUserRegister = $repository->
+     }
+
+     /**
+      * Le nombre d’utilisateur par sexe.
+     */   
+     public function numberUserBySexe (UserRepository $repository, Request $request):Response
+     {
+        $sexe = $request->query->get('sexe', 'Femme');
+        $nbUserBySexe = $repository->nombreUsersbySexe($sexe);
+
+        return $this->render('user.html.twig', ['nbUserBySexe' => $nbUserBySexe]);
+     }
+
+    /**
+     * Les nombre d’utilisateur selon le type de boite mail (gmail, yahoo, ...).
+     */
+     public function numberUserByMail (UserRepository $repository, Request $request) :Response
+     {
+         $typeMail = $request->query->get('typeMail', 'yahoo');
+         $nbUserByMail = $repository->nombreUsersbyEmailType($typeMail);
+
+        return $this->render('user.html.twig', ['nbUserByMail' => $nbUserByMail ]);
+     }
+    
+    /*
+     * nombre d'utilisateur actif
+     */
+    public function  numberActiveUsers () {
+
+    }
     
 }
--- BasePet/src/Repository/UserRepository.php
+++ BasePet/src/Repository/UserRepository.php
@@ -25,47 +25,42 @@ class UserRepository extends ServiceEntityRepository {
         ;
     }
 
-    //les users dans la meme ville 
-    public function userSameCountry($city,  $pageId = 1) {
-        return $this->createQueryBuilder('COUNT(u.email)')
-        ->from(User::class, 'u')
-        ->where('u.city= :u.city')
-        ->setParameter('u.city', $city)
-        ->setFirstResult($pageId)
-        ->setMaxResults(10)
+    //le nombre d'utilisateur par ville commun 
+    public function userSameCountry($country) {
+        return $this->createQueryBuilder('u')
+        ->select('COUNT(u.email) as numberUserByCountry, u.country')
+        ->andWhere('u.country= :country')
+        ->setParameter('country', $country)
         ->getQuery()
         ->getResult();
     }
 
     // nouveau inscrit 
-    public function newUserRegister ($pageId) {
+    public function newUserRegister () {
         return $this->createQueryBuilder('COUNT(u.iduser)')
-        ->from(User::class, 'u')
+        ->select('COUNT(u.iduser), u.createdat')
         ->where('CURRENT_DATE() >= u.createdat')
-        ->setFirstResult($pageId)
-        ->setMaxResults(10)
+        ->setParameter('','')
         ->getQuery()
         ->getResult();
         ;
     }
 
     //le nombre utilisateur par sexe 
-    public function nombreUsersbySexe ($sexe = 'Femme', $pageId = 1) {
-        return $this->createQueryBuilder('COUNT(u.iduser) as nombreUserBySexe')
+    public function nombreUsersbySexe ($sexe = 'Femme') {
+        return $this->createQueryBuilder('u')
+        ->select('COUNT(u.iduser) as nombreUserBySexe, u.sexe')
         ->andWhere('u.sexe = :sexe')
         ->setParameter('sexe', $sexe)
-        ->orderBy('u.firstname', 'ASC')
-        ->setFirstResult($pageId) //debut pagination
-        ->setMaxResults(10) // envoi 10 elem
         ->getQuery()
         ->getResult();
     }
 
     // nombre utilisateur selon le type de boite mail
     public function nombreUsersbyEmailType($typeMail) {
-        return $this->createQueryBuilder('COUNT(u.iduser) as nomberUserMailType')
-        ->from(User::class, 'u')
-        ->where('u.email LIKE :typeMail')
+        return $this->createQueryBuilder('u')
+        ->select('COUNT(u.email) as nomberUserMailType, u.email')
+        ->andWhere('u.email LIKE :typeMail')
         ->setParameter( 'typeMail', '%'.$typeMail.'%')
         ->getQuery()
         ->getResult()
